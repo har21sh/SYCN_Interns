974876584ee3c5d9ceb80b74ebee8958e13ac5d0
har21sh <126021795+har21sh@users.noreply.github.com>
2023-03-25 21:28:46 +0530

    Add files via upload

---
+++ url_shortener.py
@@ -0,0 +1,23 @@
+import requests
+def shorten_link(full_link, link_name):
+    API_KEY = 'ca627ab25cceac753e828eda536bcca44e22b'
+    BASE_URL = 'https://cutt.ly/api/api.php'
+    payload = {'key': API_KEY, 'short': full_link, 'name': link_name}
+    request = requests.get(BASE_URL, params=payload)
+    data = request.json()
+    print('')
+
+    try:
+        title = data['url']['title']
+        short_link = data['url']['shortLink']
+
+        print('Title:', title)
+        print('Link:', short_link)
+
+    except:
+        status = data['url']['status']
+        print('Error Status:', status)
+link = input('Enter a link: >>')
+name = input('Give your link a name: >>')
+
+shorten_link(link, name)
